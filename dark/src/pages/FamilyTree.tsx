e25abf9c729b634b7bcd6f2e240bb610402ebda7
alex-ironside <aleksander3wielki@gmail.com>
2026-02-07 12:27:33 +0100

    stable version 1

--- dark/src/pages/FamilyTree.tsx
+++ dark/src/pages/FamilyTree.tsx
@@ -1,11 +1,27 @@
-import { GraphCanvas } from "reagraph";
+import { GraphCanvas, useSelection, type GraphCanvasRef } from "reagraph";
 import { nodes, edges } from "./data";
+import { useRef } from "react";
 
 export function FamilyTree() {
+  const graphRef = useRef<GraphCanvasRef | null>(null);
+  const { selections, onNodeClick, onCanvasClick } = useSelection({
+    ref: graphRef,
+    nodes: nodes,
+    edges: edges,
+  });
+
   return (
     <div>
-      <GraphCanvas nodes={nodes} edges={edges} />
       <h1>Family Tree</h1>
+      <GraphCanvas
+        layoutType="forceatlas2"
+        nodes={nodes}
+        edges={edges}
+        selections={selections}
+        onCanvasClick={onCanvasClick}
+        onNodeClick={onNodeClick}
+        draggable
+      />
     </div>
   );
 }
--- dark/src/pages/data.ts
+++ dark/src/pages/data.ts
@@ -1,23 +1,102 @@
 import type { GraphEdge } from "reagraph";
 
+// silly way to ensure uniqueness
 const familyTree = [
-  {
-    id: "hannah",
-  },
-  {
-    id: "jonas",
-  },
+  "jonas",
+  "hannah",
+  "ulrich",
+  "katharina",
+  "martha",
+  "magnus",
+  "mikkel",
+  "charlotte",
+  "peter",
+  "franziska",
+  "elisabeth",
+  "helge",
+  "claudia",
+  "regina",
+  "aleksander",
+  "bartosz",
+  "egon",
+  "noah",
+  "agnes",
+  "tronte",
+  "jana",
+  "silja",
+  "doris",
+  "unknown",
+] as const;
+
+export const nodes = familyTree.map((i) => {
+  let family;
+  if (i === "hannah") {
+    family = "kahnwald";
+  }
+  return { id: i, label: i, data: { family: "Kahnwald" } };
+});
+
+type FamilyMember = (typeof familyTree)[number];
+
+// [parent, child]
+const connections: [FamilyMember, FamilyMember][] = [
+  ["hannah", "jonas"],
+  ["mikkel", "jonas"],
+
+  ["katharina", "mikkel"],
+  ["ulrich", "mikkel"],
+
+  ["katharina", "martha"],
+  ["ulrich", "martha"],
+
+  ["katharina", "magnus"],
+  ["ulrich", "magnus"],
+
+  ["regina", "bartosz"],
+  ["aleksander", "bartosz"],
+
+  ["charlotte", "elisabeth"],
+  ["peter", "elisabeth"],
+
+  ["elisabeth", "charlotte"],
+  ["noah", "charlotte"],
+
+  ["charlotte", "franziska"],
+  ["peter", "franziska"],
+
+  ["claudia", "regina"],
+  ["tronte", "regina"],
+
+  ["tronte", "ulrich"],
+  ["jana", "ulrich"],
+
+  ["egon", "claudia"],
+  ["doris", "claudia"],
+
+  ["agnes", "tronte"],
+
+  ["helge", "peter"],
+
+  ["bartosz", "noah"],
+  ["silja", "noah"],
+
+  ["bartosz", "agnes"],
+  ["silja", "agnes"],
+
+  ["hannah", "silja"],
+  ["egon", "silja"],
+
+  ["martha", "unknown"],
+  ["jonas", "unknown"],
 ];
-export const nodes = familyTree.map((i) => ({ ...i, label: i.id }));
-const connections = [
-  {
-    parent: "hannah",
-    child: "jonas",
-  },
-];
-export const edges: GraphEdge[] = connections.map((i) => ({
-  source: i.parent,
-  target: i.child,
-  id: `${i.parent}-${i.child}`,
-  label: `${i.parent}-${i.child}`,
-}));
+export const edges: GraphEdge[] = connections.map((i) => {
+  const parent = i[0];
+  const child = i[1];
+
+  return {
+    source: parent,
+    target: child,
+    id: `${parent}-${child}`,
+    label: `${parent}-${child}`,
+  };
+});
